ffc905b8b9bbd1ba080f5f22f3814253bcfa7afd
Monte Hoover <monte.b.hoover@gmail.com>
2022-05-24 11:44:46 -0400

    Run on new drill model.

--- scripts/examples/drill.sh
+++ scripts/examples/drill.sh
@@ -1,4 +1,4 @@
 python main.py --input-pc ./data/drill_pointcloud.ply \
---initial-mesh ./data/giraffe_initmesh.obj \
+--initial-mesh ./data/drill_pointcloud_hull.obj \
 --save-path ./checkpoints/drill \
 --iterations 6000
